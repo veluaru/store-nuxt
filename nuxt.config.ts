73263dc9168b5495191a2d1218854a42f06db6c5
Angie Castañeda <angie.castaneda@guane.com.co>
2025-11-28 19:20:44 -0500

    :rocket: Adding deploy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // @ts-nocheck
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/store-nuxt/'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
